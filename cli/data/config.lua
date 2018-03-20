75c6e20f117603cc938c2c5e43bd07a5bbb24994
xlovearashwm <37581985+xlovearashwm@users.noreply.github.com>
2018-03-20 16:27:19 +0330

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,46 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+ "addkick",
+ "Administrative",
+ "Auto-Lock",
+ "BanHammer",
+ "chat",
+ "clean",
+ "cleanmembers",
+ "Del-Chat",
+ "Fun",
+ "FwdMsg",
+ "Help",
+ "idprof",
+ "JoinChannel",
+ "Limitmember",
+ "Monshi",
+ "MRCore",
+ "Msg",
+ "Openchat",
+ "Payping",
+ "Plfun",
+ "Plugins",
+ "Rank",
+ "RepChatStats",
+ "Videonote",
+ "Music",
+ "Stats",
+ "core",
+ "like",
+ "Warn"
+  },
+  info_text = "\9》 TiiGeR BoT v8.8\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/BeyondTeam/BDReborn \n\n》Admins :\n》@Arashwm ➣ Founder & Developer《\n》@Arashwm ➣ Developer《\n》@Arashwm ➣ Developer《\n\n》Special thanks to :\n》TiiGeRTeM\n》@Arashwm\n\n》Our channel :\n》@TiiGeRTem《\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+  556081783,
+  502774533,
+  205549111
+  }
+}
+return _
+end
